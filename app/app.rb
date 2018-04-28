c96fa9e816d6e75bd8bf52b86b2bc73925afc0f4
Chengka <european@iqing.com>
2018-04-28 18:27:02 +0800

    user

--- app/app.rb
+++ app/app.rb
@@ -23,19 +23,20 @@ end
 #   "password": "123456",
 #   "password_confirmation": "123456"
 # }
-post '/users' do
+post '/api/users' do
   content_type :json
   payload = JSON.parse(request.body.read)
   user = User.new(name: payload['name'], password: payload['password'], password_confirmation: payload['password_confirmation'])
   if user.save
     {
       user_id: user.id,
+      name: user.name,
       token: user.token
     }.to_json
   else
     {
       error: true,
-      msg: user.errors.full_messages
+      msg: user.errors.full_messages.join(', ')
     }.to_json
   end
 end
@@ -45,20 +46,40 @@ end
 #   "user": "bydmm",
 #   "password": "123456",
 # }
-post '/users/login' do
+post '/api/users/login' do
   content_type :json
   payload = JSON.parse(request.body.read)
   user = User.find_by(name: payload['name']).try(:authenticate, payload['password'])
   if user
-    cookies[:token] = user.token
     {
       user_id: user.id,
+      name: user.name,
       token: user.token
     }.to_json
   else
     {
       error: true,
       msg: '账户或者密码错误'
+    }.to_json
+  end
+end
+
+# 获取用户
+# header "x-token": "435345435345",
+get '/api/user' do
+  content_type :json
+  token = request.env['HTTP_X_TOKEN']
+  user = User.find_by(token: token)
+  if user
+    {
+      user_id: user.id,
+      name: user.name,
+      token: user.token
+    }.to_json
+  else
+    {
+      error: true,
+      msg: '未登录'
     }.to_json
   end
 end
